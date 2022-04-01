397bf90a8a73e1953397de71f5297755661ad759
rullyafrizal <rullyafrizal4@gmail.com>
2022-04-01 20:09:16 +0700

    initial commit

---
+++ includes/sidebar.php
@@ -0,0 +1,38 @@
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index.php" class="brand-link">
+      <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+           style="opacity: .8">
+      <span class="brand-text font-weight-light">AdminKatalog</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          
+          <li class="nav-item">
+            <a href="index.php" class="nav-link">
+              <i class="nav-icon fas fa-database"></i>
+              <p>
+                Data Master
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="penjualan.php" class="nav-link">
+              <i class="nav-icon fas fa-chart-line"></i>
+              <p>
+              Transaksi Penjualan
+              </p>
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
